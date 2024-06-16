22782521d53fc8368f8e4cd4cd33ee07af43a1dd
algis398507 <algis398507@yandex.ru>
2024-06-16 22:02:22 +0300

    Тестирование процесса pipline 4

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ def start_consumer():
     # Listen to the queue and call the callback function on receiving a message
     channel.basic_consume(queue='organ_docs', on_message_callback=callback, auto_ack=True)
     # Start consuming
-    print('Началось новейшее прослушивание очереди...')
+    print('Началось новое прослушивание очереди...')
     channel.start_consuming()
 
 
